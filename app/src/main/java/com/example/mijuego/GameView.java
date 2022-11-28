cc3364255304159afdf23777ca7b6649b7215f60
pcClase <ikadh@plaiaundi.net>
2022-11-28 09:50:21 +0100

    juego terminado

--- app/src/main/java/com/example/mijuego/GameView.java
+++ app/src/main/java/com/example/mijuego/GameView.java
@@ -1,23 +1,59 @@
 package com.example.mijuego;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GameView extends SurfaceView implements Runnable{
 
     private Thread thread;
-    private boolean isPlaying;
-    private int screenX, screenY;
+    private boolean isPlaying, isGameOver = false;
+    private int screenX, screenY, score = 0;
+    private int sound;
     public static float screenRatioX, screenRatioY;
     private Paint paint;
+    private Bird[] birds;
+    private Random random;
+    private List<Bullet> bullets;
     private BackGround backGround1, backGround2;
     private Flight flight;
-    public GameView(Context context, int screenX, int screenY) {
-        super(context);
+    private SharedPreferences prefs;
+    private  GameActivity gameActivity;
+    private SoundPool soundPool;
+    public GameView(GameActivity gameActivity, int screenX, int screenY) {
+        super(gameActivity);
+
+        this.gameActivity = gameActivity;
+
+        prefs = gameActivity.getSharedPreferences("game", Context.MODE_PRIVATE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .build();
+            soundPool = new SoundPool.Builder()
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        }else {
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        }
+
+        sound = soundPool.load(gameActivity, R.raw.shoot_sound, 1);
         this.screenX = screenX;
         this.screenY = screenY;
 
@@ -27,11 +63,21 @@ public class GameView extends SurfaceView implements Runnable{
         backGround1 = new BackGround(screenX, screenY, getResources());
         backGround2 = new BackGround(screenX, screenY, getResources());
 
-        flight = new  Flight(screenY, getResources());
+        flight = new  Flight(this,screenY, getResources());
 
+        bullets = new ArrayList<>();
 
         backGround2.x = screenX;
         paint = new Paint();
+        paint.setTextSize(128);
+        paint.setColor(Color.WHITE);
+        birds = new Bird[4];
+        for (int i = 0; i < 4;i++){
+            Bird bird = new Bird(getResources());
+            birds[i] = bird;
+        }
+        random = new Random();
+
     }
 
     @Override
@@ -66,6 +112,57 @@ public class GameView extends SurfaceView implements Runnable{
             flight.y = screenY - flight.height;
         }
 
+        List<Bullet> trash = new ArrayList<>();
+        for (Bullet bullet: bullets){
+            if (bullet.x > screenX){
+                trash.add(bullet);
+            }
+            bullet.x += 50 * screenRatioX;
+
+            for (Bird bird : birds){
+                if (Rect.intersects(bird.getCollisionShape(), bullet.getCollisionShape())){
+                    score++;
+                    bird.x = -500;
+                    bullet.x = screenX + 500;
+                    bird.wasShot = true;
+                }
+            }
+
+        }
+
+
+        for (Bullet bullet: trash){
+            bullets.remove(bullet);
+        }
+
+        for (Bird bird : birds){
+            bird.x -= bird.speed;
+            if (bird.x + bird.width < 0){
+
+                if (!bird.wasShot){
+                    isGameOver = true;
+                    return;
+                }
+
+                int bound = (int) (30 * screenRatioX);
+                bird.speed = random.nextInt(bound);
+
+                if (bird.speed < 10 * screenRatioX){
+                    bird.speed = (int) (10 * screenRatioX);
+                }
+
+                bird.x = screenX;
+                bird.y = random.nextInt(screenY - bird.height);
+
+                bird.wasShot = false;
+            }
+
+            if (Rect.intersects(bird.getCollisionShape(), flight.getCollisionShape())){
+                isGameOver = true;
+                return;
+            }
+        }
+
     }
 
     private void draw(){
@@ -74,11 +171,49 @@ public class GameView extends SurfaceView implements Runnable{
             canvas.drawBitmap(backGround1.background, backGround1.x, backGround1.y, paint);
             canvas.drawBitmap(backGround2.background, backGround2.x, backGround2.y, paint);
 
+            for (Bird bird: birds){
+                canvas.drawBitmap(bird.getBird(), bird.x, bird.y, paint);
+            }
+            canvas.drawText(score + "", screenX / 2f, 164, paint);
+            
+            if (isGameOver){
+                isPlaying = false;
+                canvas.drawBitmap(flight.getDead(), flight.x, flight.y, paint);
+                getHolder().unlockCanvasAndPost(canvas);
+                saveIfHighScore();
+                waitBeforeExiting();
+                return;
+            }
+
             canvas.drawBitmap(flight.getFlight(), flight.x, flight.y, paint);
+
+            for (Bullet bullet : bullets){
+                canvas.drawBitmap(bullet.bullet, bullet.x, bullet.y, paint);
+            }
+
 
             getHolder().unlockCanvasAndPost(canvas);
         }
     }
+
+    private void waitBeforeExiting() {
+        try {
+            Thread.sleep(3000);
+            gameActivity.startActivity(new Intent(gameActivity, MainActivity.class));
+            gameActivity.finish();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveIfHighScore() {
+        if (prefs.getInt("highScore", 0 ) < score){
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("highScore", score);
+            editor.apply();
+        }
+    }
+
     private void sleep(){
         try {
             Thread.sleep(17);
@@ -112,9 +247,22 @@ public class GameView extends SurfaceView implements Runnable{
                 break;
             case MotionEvent.ACTION_UP:
                 flight.isGoingUp = false;
+                if (event.getX() > screenX / 2){
+                    flight.toShoot++;
+                }
                 break;
         }
         return true;
     }
 
+    public void newBullet() {
+        if (!prefs.getBoolean("isMute", false)){
+            soundPool.play(sound, 1, 1 ,0, 0, 1);
+        }
+        Bullet bullet = new Bullet(getResources());
+        bullet.x = flight.x + flight.width;
+        bullet.y = flight.y + (flight.height / 2);
+        bullets.add(bullet);
+
+    }
 }
--- app/src/main/java/com/example/mijuego/MainActivity.java
+++ app/src/main/java/com/example/mijuego/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.mijuego;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean isMute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,28 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.idbtnJugar).setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this, GameActivity.class));
+        });
+        TextView highScoreTxt = findViewById(R.id.idHighScoreTxt);
+        SharedPreferences prefs = getSharedPreferences("game", MODE_PRIVATE);
+
+        highScoreTxt.setText("HighScore: " + prefs.getInt("highScore",0));
+        isMute = prefs.getBoolean("isMute", false);
+        final ImageView volumeCtrl = findViewById(R.id.btnMute);
+        if (isMute){
+            volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+        }else {
+            volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+        }
+        volumeCtrl.setOnClickListener(view -> {
+            isMute = !isMute;
+            if (isMute){
+                volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+            }else {
+                volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+            }
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("isMute", isMute);
+            editor.apply();
         });
     }
 }
